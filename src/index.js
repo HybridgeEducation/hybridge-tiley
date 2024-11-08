d701f4bd6b65971de79e4aad040dd6f619d09416
BalbuenaGT <pedro03balbuena@gmail.com>
2024-11-07 19:11:13 -0700

    Add customizable color for text of avatars

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const initials = require('./lib/initials');
 const generateImage = require('./lib/generateImage');
 const generateFontSize = require('./lib/generateFontSize');
 const idToColor = require('./lib/idToColor');
-const { validateHex } = require('./lib/colors');
+const { validateHex, hexToRgb } = require('./lib/colors');
 const errorHandlingMiddleware = require('./middlewares/errorHandling');
 const argv = require('minimist')(process.argv.slice(2));
 
@@ -14,6 +14,7 @@ const app = express();
 function getColor(req) {
   if (req.query.c) {
     if (validateHex(req.query.c)) {
+      console.log('query.c', `#${req.query.c}`);
       return `#${req.query.c}`;
     }
     const error = new Error('Invalid color parameter');
@@ -21,8 +22,14 @@ function getColor(req) {
     error.status = 422;
     throw error;
   }
-
+  console.log('idToColor', idToColor(req.params.id));
   return idToColor(req.params.id);
+}
+
+function getContrastColor(bgColor) {
+  const rgb = hexToRgb(bgColor);
+  const luminance = ((0.2126 * rgb.r) + (0.7152 * rgb.g) + (0.0722 * rgb.b)) / 255;
+  return luminance > 0.5 ? '#333' : '#fff';
 }
 
 app.set('views', 'src/views');
@@ -30,28 +37,32 @@ app.set('view engine', 'ejs');
 app.use(morgan('combined'));
 
 app.get('/avatar/:id(\\w+)/:initials.:format(png|jpg)', (req, res, next) => {
-  const color = getColor(req);
+  const backgroundColor = getColor(req);
+  const color = getContrastColor(getColor(req));
   const text = initials(req.params.initials);
   const font = 'src/fonts/opensans-semibold.ttf';
   const format = req.params.format;
   const imageSize = parseInt(req.query.s, 10) || 100;
 
   res.set('Content-Type', `image/${format}`);
-  generateImage(imageSize, color, font, text, format).stream((err, stdout) => {
+  generateImage(imageSize, backgroundColor, color, font, text, format).stream((err, stdout) => {
     if (err) return next(err);
-    return stdout.pipe(res);
+    const result = stdout.pipe(res);
+    console.log('result', result);
+    return result;
   });
 });
 
 app.get('/avatar/:id(\\w+)/:initials.:format(svg)?', (req, res) => {
-  const color = getColor(req);
+  const backgroundColor = getColor(req);
+  const color = getContrastColor(getColor(req));
   const text = initials(req.params.initials);
   const imageSize = parseInt(req.query.s, 10) || 100;
   const fontSize = generateFontSize(imageSize);
 
   res.setHeader('Content-Type', 'image/svg+xml');
   res.setHeader('vary', 'Accept-Encoding');
-  res.render('svg', { color, text, imageSize, fontSize });
+  res.render('svg', { color, backgroundColor, text, imageSize, fontSize });
 });
 
 app.get('/', (req, res) => {
--- src/lib/generateImage.js
+++ src/lib/generateImage.js
@@ -3,12 +3,12 @@ const generateFontSize = require('./generateFontSize');
 
 const imageMagick = gm.subClass({ imageMagick: true });
 
-function generateImage(imageSize, color, font, text, format) {
-  return imageMagick(imageSize, imageSize, color)
-  .fill('#fff')
-  .font(font, generateFontSize(imageSize))
-  .drawText(-1, -3, text, 'Center')
-  .setFormat(format);
+function generateImage(imageSize, backgroundColor, color, font, text, format) {
+  return imageMagick(imageSize, imageSize, backgroundColor)
+    .fill(color)
+    .font(font, generateFontSize(imageSize))
+    .drawText(-1, -3, text, 'Center')
+    .setFormat(format);
 }
 
 module.exports = generateImage;
